2513415a19340019cc95dbf1abe8edf5ab7c3731
Sea Eun Lee <seaeunlee@microsoft.com>
2023-11-09 10:10:07 -0800

    allow domain prefix

--- api/subscriptions/index.js
+++ api/subscriptions/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     context.log('2 JavaScript HTTP trigger function processed a request for URL ${req.url}".');
 
-    const urlRegex = /^\/arm\/api\/subscriptions\/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\?api-version=2.0$/;
+    const urlRegex = /^(https?:\/\/)?[a-zA-Z0-9.-]+\/arm\/api\/subscriptions\/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\?api-version=2.0$/;
     if (req.method === 'PUT' && urlRegex.test(req.url)) {
         const requestBody = req.body;
         context.res = {
