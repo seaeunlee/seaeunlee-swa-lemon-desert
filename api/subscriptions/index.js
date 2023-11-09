35cc2cac149618857473253dcbac5e60e3cd124d
Sea Eun Lee <seaeunlee@microsoft.com>
2023-11-09 10:24:15 -0800

    fix regex

--- api/subscriptions/index.js
+++ api/subscriptions/index.js
@@ -1,7 +1,8 @@
 module.exports = async function (context, req) {
     context.log('2 JavaScript HTTP trigger function processed a request for URL ${req.url}".');
 
-    const urlRegex = /^(https?:\/\/)?[a-zA-Z0-9.-]+\/arm\/api\/subscriptions\/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\?api-version=2.0$/;
+    const urlRegex = /^(https?:\/\/)?[a-zA-Z0-9.:\-]+\/arm\/api\/subscriptions\/[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}\?api-version=2.0$/;
+    //                  https://gray-water-0de...net/arm/api/subscriptions/7d589e74-608d-4ec3-8fbf-168c6894a003?api-version=2.0                  
     if (req.method === 'PUT' && urlRegex.test(req.url)) {
         const requestBody = req.body;
         context.res = {
