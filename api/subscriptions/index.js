eadf766ae5bf5872f39b198859034d7973ce7a0f
Sea Eun Lee <seaeunlee@microsoft.com>
2023-11-09 15:16:30 -0800

    test 404 for all

--- api/subscriptions/index.js
+++ api/subscriptions/index.js
@@ -1,16 +1,8 @@
 module.exports = async function (context, req) {
     context.log('3 JavaScript HTTP trigger function processed a request for URL.', req.url);
 
-    if (req.method === 'PUT') {
-        const requestBody = req.body;
-        context.res = {
-            status: 200,
-            body: requestBody
-        }
-    } else {
-        context.res = {
-            status: 404,
-            body: 'Not Found'
-        }
+    context.res = {
+        status: 404,
+        body: 'Not Found'
     }
 }
