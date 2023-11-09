80eef7baf1d104eef04fc79ded9aec297e493ca7
Sea Eun Lee <seaeunlee@microsoft.com>
2023-11-08 16:44:03 -0800

    only echo on put

--- api/subscriptions/index.js
+++ api/subscriptions/index.js
@@ -1,8 +1,15 @@
 module.exports = async function (context, req) {
     context.log('1 JavaScript HTTP trigger function processed a request for URL "${req.url}".');
 
-    context.res = {
-        status: 200,
-        body: req.body
+    if (req.method === 'PUT') {
+        const requestBody = req.body;
+        context.res = {
+            status: 200,
+            body: requestBody
+        }
+    } else {
+        context.res = {
+            status: 200,
+        }
     }
 }
