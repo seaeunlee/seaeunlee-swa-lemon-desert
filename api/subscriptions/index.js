b1c2675413c0f81b6a4ad13a221893b9ba4db0a9
Sea Eun Lee <seaeunlee@microsoft.com>
2023-11-09 10:03:17 -0800

    GUID regex

--- api/subscriptions/index.js
+++ api/subscriptions/index.js
@@ -1,7 +1,8 @@
 module.exports = async function (context, req) {
     context.log('2 JavaScript HTTP trigger function processed a request for URL ${req.url}".');
 
-    if (req.method === 'PUT') {
+    const urlRegex = /^\/arm\/api\/subscriptions\/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\?api-version=2.0$/;
+    if (req.method === 'PUT' && urlRegex.test(req.url)) {
         const requestBody = req.body;
         context.res = {
             status: 200,
@@ -9,7 +10,8 @@ module.exports = async function (context, req) {
         }
     } else {
         context.res = {
-            status: 200,
+            status: 404,
+            body: 'Not Found'
         }
     }
 }
