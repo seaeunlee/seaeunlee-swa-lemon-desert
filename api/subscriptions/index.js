8a306d546746c9e066927078774026f8efdc5887
Sea Eun Lee <seaeunlee@microsoft.com>
2023-11-08 18:02:04 -0800

    modify config

--- api/subscriptions/index.js
+++ api/subscriptions/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('1 JavaScript HTTP trigger function processed a request for URL "${req.url}".');
+    context.log('2 JavaScript HTTP trigger function processed a request for URL ${req.url}".');
 
     if (req.method === 'PUT') {
         const requestBody = req.body;
